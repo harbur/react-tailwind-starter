470571cdafe701c887dba43e75a82872a6afa6dd
spiddy <dimitris.kapanidis@harbur.io>
2020-05-28 01:51:41 +0200

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import history from './history/history';
 import Shell from './layouts/Shell';
+import { Segment } from 'semantic-ui-react';
+import Home from './pages/home/Home';
+import ListMovies from './pages/movies/ListMovies';
+import ViewMovie from './pages/movies/ViewMovie';
+import CreateMovie from './pages/movies/CreateMovie';
+import EditMovie from './pages/movies/EditMovie';
 
 function App() {
   return (
     <Router history={history}>
-      <Shell/>
+      <Shell>
+      <Segment basic>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies" component={ListMovies} />
+          <Route exact path="/movies/:id" component={ViewMovie} />
+          <Route exact path="/create/movie" component={CreateMovie} />
+          <Route exact path="/edit/movie/:id" component={EditMovie} />
+        </Switch>
+      </Segment>
+
+        </Shell>
     {/* <div className="App">
       <SidebarExampleVisible/>
     </div> */}
--- src/layouts/Shell.tsx
+++ src/layouts/Shell.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import Transition from './Transition';
+import { NavLink } from 'react-router-dom';
 
-export default function Shell() {
+export default function Shell(props: {children: any}) {
   const [showProfile, setShowProfile] = useState(false)
-  console.log(showProfile)
+  const [showMenu, setShowMenu] = useState(false)
+  console.log(showMenu)
   return (
     <div>
       <nav className="bg-gray-800">
@@ -15,11 +17,8 @@ export default function Shell() {
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline">
-                  <a href="#" className="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">Dashboard</a>
-                  <a href="#" className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Team</a>
-                  <a href="#" className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Projects</a>
-                  <a href="#" className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Calendar</a>
-                  <a href="#" className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Reports</a>
+                  <NavLink to='/home' activeClassName="text-white bg-gray-900" className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Home</NavLink>
+                  <NavLink to='/movies' activeClassName="text-white bg-gray-900"  className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Movies</NavLink>
                 </div>
               </div>
             </div>
@@ -38,17 +37,16 @@ export default function Shell() {
                     </button>
                   </div>
                   <Transition
-        show={showProfile}
-        enter="transition ease-out duration-100 transform"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100"
-        leave="transition ease-in duration-75 transform"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-75"
-      >
+                    show={showProfile}
+                    enter="transition ease-out duration-100 transform"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="transition ease-in duration-75 transform"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-75"
+                  >
 
 
-                  
                     <div className={`origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg`}>
                       <div className="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
                         <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
@@ -56,17 +54,16 @@ export default function Shell() {
                         <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
                       </div>
                     </div>
-                    </Transition>
-
+                  </Transition>
                 </div>
               </div>
             </div>
             <div className="-mr-2 flex md:hidden">
-              <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white">
-                <svg className="block h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <button onClick={() => setShowMenu(!showMenu)} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white">
+                <svg className={`${showMenu ? 'hidden' : 'block'} h-6 w-6`} stroke="currentColor" fill="none" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
-                <svg className="hidden h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <svg className={`${showMenu ? 'block' : 'hidden'} h-6 w-6`} stroke="currentColor" fill="none" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
@@ -74,7 +71,7 @@ export default function Shell() {
           </div>
         </div>
 
-        <div className="hidden md:hidden">
+        <div className={`${showMenu ? 'block' : 'hidden'} md:hidden`}>
           <div className="px-2 pt-2 pb-3 sm:px-3">
             <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">Dashboard</a>
             <a href="#" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Team</a>
@@ -111,7 +108,9 @@ export default function Shell() {
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
+            {props.children}
+            </div>
           </div>
         </div>
       </main>
