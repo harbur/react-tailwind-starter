04d6ff4ff6794a16d6623cb797da5873af45ad35
spiddy <dimitris.kapanidis@harbur.io>
2020-09-16 01:16:23 +0200

    added @apply examples

--- src/layouts/Shell.tsx
+++ src/layouts/Shell.tsx
@@ -16,8 +16,8 @@ export default function Shell(props: {children: any}) {
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline">
-                  <NavLink to='/' activeClassName="text-white bg-gray-900" className="px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Home</NavLink>
-                  <NavLink to='/movies' activeClassName="text-white bg-gray-900"  className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Movies</NavLink>
+                  <NavLink exact to='/' className="nav-btn">Home</NavLink>
+                  <NavLink to='/movies' className="nav-btn">Movies</NavLink>
                 </div>
               </div>
             </div>
