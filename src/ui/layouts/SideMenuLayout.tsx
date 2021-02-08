02995dfe64d035bb42ef88cfc84606108f9038a9
spiddy <dimitris.kapanidis@harbur.io>
2021-02-08 19:19:55 +0100

    wip

--- src/ui/layouts/SideMenuLayout.tsx
+++ src/ui/layouts/SideMenuLayout.tsx
@@ -9,7 +9,7 @@ export default function SideMenuLayout({ children }: SideMenuLayoutProps) {
   const [value, setValue] = useLocalStorage('sidemenu.size', 200);
   return (
     <div className="flex h-screen">
-      <SplitPane onDragFinished={setValue} split="vertical" minSize={120} defaultSize={value} maxSize={400}>
+      <SplitPane onDragFinished={setValue} split="vertical" minSize={130} defaultSize={value} maxSize={400}>
         <Menu />
         <div className="p-4">
           {children}
@@ -22,9 +22,9 @@ export default function SideMenuLayout({ children }: SideMenuLayoutProps) {
 // Menu at the top with navigation buttons
 function Menu() {
   return (
-    <nav className="bg-gray-900 w-full flex flex-col h-full pt-10">
-      <NavButton icon={<MdHome className="text-gray-500" />} exact to='/' title="Home" />
-      <NavButton icon={<MdLocalMovies className="text-gray-500" />} to='/movies/' title="Movies" />
+    <nav className="bg-gray-900 w-full flex flex-col h-full pt-10 text-gray-500">
+      <NavButton icon={<MdHome className="text-current" />} exact to='/' title="Home" />
+      <NavButton icon={<MdLocalMovies className="text-current" />} to='/movies/' title="Movies" />
     </nav>
   )
 }
@@ -32,9 +32,9 @@ function Menu() {
 // navigation button on menu
 interface NavButtonProps { to: string, title: string, exact?: boolean, icon?: any }
 function NavButton({ exact = false, to, title, icon }: NavButtonProps) {
-  return <NavLink activeClassName="bg-gray-800" exact={exact} to={to} className="
-  my-0.5 mx-2 px-3 py-0.5 rounded-md text-sm font-medium text-gray-300
-  hover:text-white hover:bg-gray-800 flex items-center
-  focus:outline-none focus:text-white focus:bg-gray-800 space-x-1
-">{icon}<span>{title}</span></NavLink>
+  return <NavLink activeClassName="text-blue-400  bg-gray-800" exact={exact} to={to} className="
+  my-1 mx-4 px-3 py-2 rounded-lg text-sm font-medium
+ hover:bg-gray-800 flex items-center
+  focus:outline-none space-x-1
+">{icon}<span className="text-gray-300">{title}</span></NavLink>
 }
